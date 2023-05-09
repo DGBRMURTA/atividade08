36c44d2ce189e9ae0663c191e3512424ff46b91a
DGBRMURTA <110132975+DGBRMURTA@users.noreply.github.com>
2023-05-09 20:48:32 -0300

    Delete avaliacao2 directory

--- avaliacao2/javascript.js
+++
@@ -1,9 +0,0 @@
-function resetar(){
-    var elm = document.getElementsByClassName('elemento')
-    var elmlen = elm.length
-    for(i=0;i<elmlen;i++){
-        if(elm[i].innerHTML !== ""){
-            alert('...')
-        }
-    }
-}
